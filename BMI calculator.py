0038ee118628f5eca07875291495aac01d2d34d5
staurakis <staurakis@gmail.com>
2020-09-25 11:13:59 +0300

    Minor changes on output text

--- BMI calculator.py
+++ BMI calculator.py
@@ -1,12 +1,11 @@
 name = input("Please enter your name: ")
 height_m = float(input("Please enter your height in meters: "))
 weight_kg = float(input("Please enter your weight in kg: "))
-
 def bmifunc(name, height_m, weight_kg):
     bmi = weight_kg / (height_m ** 2)
     print(" ")
     print ("The results of the input data and the calculations are the following: ")
-    print("BMI :",bmi)
+    print(name, "your Body Mass Index (BMI) is :",bmi)
     if bmi < 25:
         if bmi < 18.5:
             return name + " you are underweight"
