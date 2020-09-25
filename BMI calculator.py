a470f83920a1ee6eac51f94c3862f7b08451f3b0
staurakis <staurakis@gmail.com>
2020-09-25 10:57:16 +0300

    Removed old code/comments

--- BMI calculator.py
+++ BMI calculator.py
@@ -1,9 +1,6 @@
 name = input("Please enter your name: ")
 height_m = float(input("Please enter your height in meters: "))
 weight_kg = float(input("Please enter your weight in kg: "))
-# name = "Thodoris"
-# height_m = 1.69
-# weight_kg = 45
 
 def bmifunc(name, height_m, weight_kg):
     bmi = weight_kg / (height_m ** 2)
